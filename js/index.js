342f1ef273b4737db67187f7eac9aba10d2aa240
HughZhang98 <40616309+HughZhang98@users.noreply.github.com>
2023-04-19 03:43:28 +1000

    Update login verification, account page, FAQ management page
    Update login verification, user account page and admin FAQ management page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+window.onload = function () {
+  let n = getCookie("username");
+  console.log(document.cookie)
+  if (n == null || n == undefined || n == '') {
+    document.getElementById("username").innerHTML = "Login";
+    document.getElementById("nav_login_ch").innerHTML = "Login";
+  } else {
+    document.getElementById("username").innerHTML = n;
+    document.getElementById("nav_login_ch").innerHTML = n;
+  }
+}
 //page
 function toHome() {
   window.location.href = "../index.html";
@@ -14,12 +25,23 @@ function toBook() {
 }
 
 function toLog() {
-  window.location.href = "../pages/Log.html";
+  let n = getCookie("username");
+  if (n == "admin") {
+    toAdBookings()
+  } else if (n == null || n == undefined || n == '') {
+    window.location.href = "../pages/Log.html";
+  } else {
+    window.location.href = "../pages/account.html"
+  }
 }
 
 function toLogout() {
-  alert("Success!")
-  window.location.href = "../index.html";
+  let logout =confirm("Are you sure you want to Log out?");
+	if (logout==true){
+		alert("Success!")
+    setCookie("username","","",-1);
+    window.location.href = "../index.html";
+	}
 }
 
 function toAdBookings () {
@@ -37,21 +59,63 @@ function toAdfaq () {
   window.location.href = "../pages/admin_FAQ.html"
 }
 
+//Cookie
+//getCookie
+function getCookie(uinfo){
+	var str = document.cookie;
+	var arr = str.split(';');
+	for(let i=0; i<arr.length; i++) {
+		let ar = arr[i].trim().split("=");
+		if (ar[0] == uinfo) {
+      return ar[1]
+    }
+	}
+	return "";
+}
+//checkCookie
+// function checkCookie(uname,upass) {
+//   let n = getCookie(uname);
+//   let p = getCookie(upass);
+
+//   if (n == null || n == undefined || n == '') {
+//     alert("Don't have this account, please create an account")
+//   } else {
+
+//   }
+// }
+//setCookie
+function setCookie(uname,uemail,upass, exdays){
+  var d = new Date();
+  d.setTime(d.getTime()+(exdays*24*60*60*1000));
+  var expires = "expires="+d.toGMTString();
+	document.cookie = "username="+uname+ "; " + expires;
+  document.cookie = "email="+uemail+ "; " + expires;
+  document.cookie = "password="+upass+ "; " + expires;
+  document.cookie = uname+"="+upass;
+}
+
 //sub question
 function sub_que() {
-  alert ("Success")
+  alert ("Success!")
 }
 //sub booking
 function sub_booking() {
-  date = document.getElementById("book_date").value;
-  if (date == null || date == undefined || date == '') {
-    alert("Please select a date!")
-  } else {
-    alert(date)
+  let uname = getCookie("username");
+  if (uname == null || uname == undefined || uname == ''){
+		toLog()
+	} else {
+    date = document.getElementById("book_date").value;
+    if (date == null || date == undefined || date == '') {
+      alert("Please select a date!")
+    } else {
+      alert(date)
+      toHome()
+    }
   }
 }
 //log in
 function sub_login() {
+
   email = document.getElementById("login_email").value;
   pass = document.getElementById("login_pass").value;
   
@@ -62,12 +126,29 @@ function sub_login() {
   } else {
     if (email == "admin" & pass == "admin") {
       alert("Welcome "+email);
+      setCookie(email,email,pass,0.0104);
       setTimeout(2500);
       window.location.href = "../pages/admin_book.html";
     } else {
-      alert("Welcome "+email);
-      setTimeout(2500);
-      window.location.href = "../index.html";
+      let nemail = getCookie(email);
+      let n = getCookie("username");
+      let p = getCookie("password");
+
+      if (n == email && p == pass) {
+        alert("Welcome "+ n);
+        setTimeout(2500);
+        window.location.href = "../index.html";
+      } else if (nemail == null || nemail == undefined || nemail == '') {
+        alert("Don't have this account, please create an account")
+      } else if (nemail != null || nemail != undefined || nemail != '' & pass == p){
+        alert("Welcome "+ nemail);
+        setCookie(email,email,nemail,0.0104);
+      } else{
+        alert("You have entered a wrong password")
+      }
+    
+      
+      
       // setTimeout (function () {
         // window.alert=function(){}
       //   window.location.href = "../index.html";
@@ -75,6 +156,16 @@ function sub_login() {
     }
   }
 }
+// //cookie prit
+// function print() {
+//   var st = document.cookie;
+//   var ar = st.split(";");
+
+//   for (let i = 0; i < ar.length; i++) {
+//     var arr = ar[i].split("=");
+//     console.log(arr[0]+": "+arr[1]);
+//   }
+// }
 //sign up
 function sub_signup() {
   uname = document.getElementById("signup_name").value;
@@ -82,6 +173,7 @@ function sub_signup() {
   pass = document.getElementById("signup_pass").value;
   
   if (uname == null || uname == undefined || uname == '') {
+
     alert("Please enter your vaild email!")
   } else if (email == null || email == undefined || email == ''){
     alert("Please enter your vaild password!")
@@ -89,8 +181,20 @@ function sub_signup() {
     alert("Please enter your vaild password!")
   } else {
     alert("Thanks for joining! Welcome "+ uname);
+    setCookie(uname,email,pass,0.0104 );
     setTimeout(2500)
     window.location.href = "../index.html";
   }
 
+}
+
+//user delete booking
+function userDelBook(){
+	let del =confirm("Are you sure you want to delete the Booking?");
+	if (del==true){
+    alert ("Success");
+    setTimeout(2000);
+    document.getElementById("bitem1").style.display = "none";
+	}
+	
 }
